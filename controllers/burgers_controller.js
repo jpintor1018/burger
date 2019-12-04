22c5d7f43293d31dd87fd1b415f022a6afa1bcc7
John Michael Pintor <jpintor1018@yahoo.com>
2019-12-04 18:48:27 -0500

    Fixed Controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,55 +1,32 @@
-var express = require("express");
-var router = express.Router();
-var burger = require("../models/burger.js");
+const express = require("express")
+const burger = require("../models/burger.js")
+const router = express.Router()
 
-router.get("/", function(req, res) {
-  burger.all(function(data) {
-    var hbsObject = {
-      burgers: data
-    };
-    console.log(hbsObject);
-    res.render("index", hbsObject);
-  });
-});
+router.get("/", (req, res) => {
+    burger.all((data) => {
+        var hbsObject = {
+            burgers: data
+        }
+        console.log(hbsObject)
+        res.render("index", hbsObject)
+    })
+})
 
-router.post("/api/burgers", function(req, res) {
-  burger.create([
-    "burger_name", "devoured"
-  ], [
-    req.body.name, req.body.devoured
-  ], function(result) {
-    res.json({ id: result.insertId });
-  });
-});
+router.post("/api/burgers", (req, res) => burger.create(req.body.name, (data) => res.json({id: data.insertID})))
 
-router.put("/api/burgers/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
+router.put("/api/burgers/:id", (req, res) => {
+    var condition = "id = " + req.params.id
+    console.log("condition: " + condition)
+    console.log(req.body.devoured)
+    
+    burger.update(req.body.devoured, condition, (result) => {
+        if (result.changedRows == 0) {
+            return res.status(404).end()
+        }
+        else{
+            res.status(200).end()
+        }
+    })
+})
 
-  console.log("condition", condition);
-
-  burger.update({
-    devoured: req.body.devoured
-  }, condition, function(result) {
-    if (result.changedRows == 0) {
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
-  });
-});
-
-router.delete("/api/burgers/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
-
-  burger.delete(condition, function(result) {
-    if (result.affectedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
-  });
-});
-
-// Export routes for server.js to use.
-module.exports = router;
+module.exports = router
