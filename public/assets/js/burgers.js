b437a24a02913db1ac2f28a5c27c50687bb3b2aa
John Michael Pintor <jpintor1018@yahoo.com>
2019-12-04 19:09:39 -0500

    Updated

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,21 +1,10 @@
-// Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
-    $(".create-form").on("submit", function(event) {
-      
-        // Make sure to preventDefault on a submit event.
-        event.preventDefault();
-    
-        var newBurger = {
-          burger_name: $("#newBurger").val().trim(),
-          devoured: 0
-        };
-
     $(".change-devoured").on("click", function(event) {
       var id = $(this).data("id");
       var newDevoured = $(this).data("newBurger");
   
       var newDevouredState = {
-        devoured: "true"
+        devoured: !newDevoured
       };
   
       // Send the PUT request.
@@ -30,7 +19,17 @@ $(function() {
         }
       );
     });
-  
+
+
+      $(".create-form").on("submit", function(event) {
+        
+          // Make sure to preventDefault on a submit event.
+          event.preventDefault();
+      
+          var newBurger = {
+            burger_name: $("#newBurger").val().trim(),
+          }
+
     
   
       // Send the POST request.
